c906314ba19034cfa66ce9a3086171de58cd603b
kathrynalpert <kathrynialpert@gmail.com>
2012-12-21 08:53:31 -0700

    commit static site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kathrynalpert::Application.config.secret_token = 'edd30846fdaa64c54ff193981bfaef62d6f3318e162401ecb2267efbb9417a0c2809c43779adf5874b5b1f11dc99f65dee6c3192bd4c64b535dae458c3fac283'
